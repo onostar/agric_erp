21591a599021c62ea1d7c5df968b78aab1da0f70
onostar <onostarkels@gmail.com>
2025-10-06 10:17:14 +0100

    updated reverse transaction for labour cost

--- controller/reverse_transaction.php
+++ controller/reverse_transaction.php
@@ -19,6 +19,19 @@
             $check_column = new selects();
             $cols = $check_column->fetch_column($table->table_name, 'trx_number');
             if($cols){
+                if($table->table_name == 'labour_payments'){
+                    //get task id
+                    $tasks = $check_column->fetch_details_group('labour_payments', 'task', 'trx_number', $trx_number);
+                    foreach($tasks as $task){
+                        $task_id = $task->task;
+                    }
+                    //update payment status on task table
+                    $data = array(
+                        'payment_status' => 0
+                    );
+                    $update_task = new Update_table();
+                    $update_task->updateAny('tasks', $data, 'task_id', $task_id);
+                }
                 $delete_tx = new deletes();
                 $delete_tx->delete_item($table->table_name, 'trx_number', $trx_number);
             }
