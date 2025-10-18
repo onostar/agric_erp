2e307be8224be0edd17f106de0b65830e5799523
onostar <onostarkels@gmail.com>
2025-10-18 06:55:03 +0100

    post production

--- controller/delete_production_material.php
+++ controller/delete_production_material.php
@@ -1,5 +1,10 @@
 <?php
-    
+    session_start();
+    date_default_timezone_set("Africa/Lagos");
+    $date = date("Y-m-d H:i:s");
+    $user = $_SESSION['user_id'];
+    $trans_type = "production delete";
+
     // if(isset($_GET['id'])){
     //     $id = $_GET['id'];
         $production = $_GET['production_id'];
@@ -9,26 +14,23 @@
         include "../classes/select.php";
         include "../classes/delete.php";
         include "../classes/update.php";
+        include "../classes/inserts.php";
 // echo $item;
         //get item details
         $get_item = new selects();
         $row = $get_item->fetch_details_group('items', 'item_name', 'item_id', $item);
         $name = $row->item_name;
         //get invoice and quantity
-        $get_invoice = new selects();
-        $rows = $get_invoice->fetch_details_cond('production', 'product_id', $production);
+        $rows = $get_item->fetch_details_cond('production', 'product_id', $production);
         foreach($rows as $row){
             $invoice = $row->product_number;
             $quantity = $row->raw_quantity;
             $store = $row->store;
+            $product = $row->product;
         }
-        //get product from the invoice
-        $get_product = new selects();
-        $prd = $get_product->fetch_details_group('production', 'product', 'product_number', $invoice);
-        $product = $prd->product;
+       
         //get previous quantity in inventory
-        $get_inv = new selects();
-        $invs = $get_inv->fetch_details_2cond('inventory', 'item', 'store', $item, $store);
+        $invs = $get_item->fetch_details_2cond('inventory', 'item', 'store', $item, $store);
         foreach($invs as $inv){
             $prev_qty = $inv->quantity;
         }
@@ -41,6 +43,19 @@
             //update inventory
             $update_inventory = new Update_table();
             $update_inventory->update2cond('inventory', 'quantity', 'store', 'item', $new_qty, $store, $item);
+            //data to insert into audit trail
+            $audit_data = array(
+                'item' => $item,
+                'transaction' => $trans_type,
+                'previous_qty' => $prev_qty,
+                'quantity' => $quantity,
+                'posted_by' => $user,
+                'store' => $store,
+                'post_date' => $date
+            );
+            
+            $inser_trail = new add_data('audit_trail', $audit_data);
+            $inser_trail->create_data();
 
 ?>
 <!-- display items with same invoice number -->
