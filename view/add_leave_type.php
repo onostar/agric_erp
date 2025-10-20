52a8f9ee243dacf9ee959ed7b05b97a30a68a982
onostar <onostarkels@gmail.com>
2025-10-20 19:45:09 +0100

    leave started

---
+++ view/add_leave_type.php
@@ -0,0 +1,35 @@
+<?php
+    include "../classes/dbh.php";
+    include "../classes/select.php";
+    session_start();
+    $farm = $_SESSION['store_id'];
+?>
+
+<div id="crop_cycle" class="displays">
+        <a style="background:brown; color:#fff; padding:5px 8px; border-radius:15px; border:1px solid #fff; box-shadow:1px 1px 1px #222;" href="javascript:void(0)" onclick="showPage('leave_types.php')" title="Return fot farm fields">Return <i class="fas fa-angle-double-left"></i></a>
+
+    <div class="info" style="width:40%; margin:20px"></div>
+    <div class="add_user_form" style="width:40%; margin:0px">
+        <h3 style="background:var(--tertiaryColor)">Create a New Leave type</h3>
+        <!-- <form method="POST" id="addUserForm"> -->
+        <section>
+            <div class="inputs" style="gap:.8rem; align-items:flex_end; justify-content:left;">
+                <div class="data" style="width:100%">
+                    <label for="title">Leave Title</label>
+                    <input type="text" id="title" name="title" placeholder="Input leave title" required>
+                </div>
+                <div class="data" style="width:100%;">
+                   <label for="max_days"> Maximum Days</label>
+                   <input type="number" name="max_days" id="max_days" required>
+                </div>
+                <div class="data" style="width:100%;">
+                   <label for="description">Description</label>
+                   <input type="text" name="description" id="description"  placeholder="Enter Leave description">
+                </div>
+                <div class="data">
+                    <button type="button" id="add_item" name="add_item" onclick="addLeave()">Save record <i class="fas fa-save"></i></button>
+                </div>
+            </div>
+        </section>    
+    </div>
+</div>
---
+++ view/leave_types.php
@@ -0,0 +1,78 @@
+<div id="cycles">
+<?php
+
+    include "../classes/dbh.php";
+    include "../classes/select.php";
+
+
+?>
+<div class="displays allResults" id="crop_cycle" style="width:90%!important;margin:20px 50px!important">
+    <h2>Active Leave Types</h2>
+    <hr>
+    <div class="search">
+        <input type="search" id="searchStaff" placeholder="Enter keyword" onkeyup="searchData(this.value)"> <a style="background:var(--tertiaryColor); color:#fff; padding:4px; margin:5px; border-radius:15px; border:1px solid #fff; box-shadow:1px 1px 1px #222;" href="javascript:void(0)" onclick="showPage('add_leave_type.php')" title="Add Leave Type">Add Leave Type <i class="fas fa-plus-square"></i></a>
+    </div>
+    <table id="room_list_table" class="searchTable">
+        <thead>
+            <tr style="background:var(--moreColor)">
+                <td>S/N</td>
+                <td>Title</td>
+                <td>Max. Days</td>
+                <td>Description</td>
+                <td>Status</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $n = 1;
+                $get_details = new selects();
+                $details = $get_details->fetch_details_order('leave_types', 'leave_title');
+                if(gettype($details) === 'array'){
+                foreach($details as $detail):
+            ?>
+            <tr>
+                <td style="text-align:center; color:red;"><?php echo $n?></td>
+                <td style="color:var(--primaryColor)">
+                    <?php 
+                        
+                        echo $detail->leave_title;
+                    ?>
+                </td>
+                <td>
+                    <?php 
+                        $detail->max_days
+                    ?>
+                </td>
+                <td><?php echo $detail->description?></td>
+                <td>
+                    <?php
+                        if($detail->leave_status == 0){
+                            echo "<span style='color:var(--tertiaryColor)'>Active <i class='fas fa-check-double'></i></span>";
+                        }else{
+                            echo "<span style='color:red'>Deactivated <i class='fas fa-ban'></i></span>";
+                        }
+                    ?>
+                </td>
+                <td>
+                    <a href="javascript:void(0)" title="edit leave details" style="color:#fff; background:var(--otherColor); padding:5px; font-size:.8rem; border-radius:15px; border:1px solid #fff; box-shadow:1px 1px 1px #222" onclick="showPage('edit_leave_type.php?cycle=<?php echo $detail->leave_id?>')"><i class="fas fa-edit"></i></a>
+                    <?php if($detail->leave_status == 0){?>
+                    <a href="javascript:void(0)" title="Disable Leave" style="color:silver; font-size:1rem;" onclick="disableLeave('<?php echo $detail->leave_id?>')"><i class="fas fa-toggle-off"></i></a>
+                    <?php }else{?>
+                    <a href="javascript:void(0)" title="Activate Leave" style="color:green; font-size:1rem;" onclick="activateLeave('<?php echo $detail->leave_id?>')"><i class="fas fa-toggle-on"></i></a>
+                    <?php }?>
+                </td>
+                
+            </tr>
+            
+            <?php $n++; endforeach;}?>
+        </tbody>
+    </table>
+    
+    <?php
+        if(gettype($details) == "string"){
+            echo "<p class='no_result'>'$details'</p>";
+        }
+    ?>
+</div>
+</div>
